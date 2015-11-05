b77bd42c0ed9078cc2df2e7ca837930dd1137ac9
Ta Minh Tien <taminhtien1993@gmail.com>
2015-11-05 14:40:27 +0700

    Add test for failure context of create vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,15 +3,28 @@ class VotesController < ApplicationController
 	end
 
 	def create
-		update_current_joke
-		mark_voted_joke
-		redirect_to_next_unvoted_joke
+		@vote = Vote.new(vote_params)
+		if @vote.save
+			update_current_joke
+			mark_voted_joke
+			redirect_to_next_unvoted_joke
+		else
+			render :new
+		end
 	end
 
 	def goodbye
 	end
-	
+
 	private
+
+		def joke_id
+			params[:joke_id]
+		end
+
+		def vote_params
+			params.require(:vote).permit(:like).merge!(joke_id: joke_id)
+		end
 
 		def current_joke
 			Joke.find(params[:joke_id])
@@ -21,6 +34,7 @@ class VotesController < ApplicationController
 			if params[:vote][:like]
 				current_joke.increase_like
 			else
+				ap params[:vote][:like], plain: true
 				current_joke.increase_dislike
 			end
 		end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,26 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
-  context "#create" do
+  describe "#create" do
     def do_request(params)
       post :create, joke_id: joke.id, vote: params
     end
 
     context "Success" do
       let!(:joke) { create(:joke) }
-      
+
+      it "saves a vote" do
+        params = attributes_for(:vote)      
+        expect { do_request(params) }.to change(Vote, :count).by(1)
+      end
+
       it "increases the number of joke's like" do
         params = attributes_for(:vote)
         expect {
           do_request(params)
-          joke.reload }.to change(joke, :like).by(1)
+          joke.reload
+        }.to change(joke, :like).by(1)
       end
 
       it "increases the number of joke's dislike" do
         params = attributes_for(:vote, like: false)
         expect {
           do_request(params)
-          joke.reload }.to change(joke, :dislike).by(1)
+          joke.reload
+        }.to change(joke, :dislike).by(1)
       end
 
       describe '#redirect_to_next_unvoted_joke' do
@@ -41,6 +48,20 @@ RSpec.describe VotesController, type: :controller do
             expect(response).to redirect_to goodbye_votes_url
           end
         end
+      end
+    end
+
+    context 'Failure' do
+      let!(:joke) { create(:joke) }
+      it 'does not save a vote' do
+        params = attributes_for(:vote, like: nil)
+        expect { do_request(params) }.not_to change(Vote, :count)
+      end
+
+      it 'renders a form if can not save a vote' do
+        params = attributes_for(:vote, like: nil)
+        do_request(params)
+        expect(response).to render_template :new
       end
     end
   end
