f6cdf31b61a4222cd89873a5320f0dbe6c1ba59e
Ta Minh Tien <taminhtien1993@gmail.com>
2015-11-05 13:55:54 +0700

    Add test for redirecting to goodbye page

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -22,6 +22,26 @@ RSpec.describe VotesController, type: :controller do
           do_request(params)
           joke.reload }.to change(joke, :dislike).by(1)
       end
+
+      describe '#redirect_to_next_unvoted_joke' do
+        let!(:params) { attributes_for(:vote) }
+
+        context 'There are unvoted jokes' do
+          let!(:next_joke) { create(:joke) }
+
+          it "redirects to next unvoted joke" do
+            do_request(params)
+            expect(response).to redirect_to new_joke_vote_url(joke_id: next_joke.id)
+          end
+        end
+
+        context 'There are no more unvoted jokes' do
+          it "redirects to goodbye page" do
+            do_request(params)
+            expect(response).to redirect_to goodbye_votes_url
+          end
+        end
+      end
     end
   end
 end
