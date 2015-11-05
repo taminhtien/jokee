db0744cd319106fa5bf1fe7abe7dc0882acf5aae
Ta Minh Tien <taminhtien1993@gmail.com>
2015-11-05 16:00:08 +0700

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Joke.delete_all
+[
+	%{
+	 	A child asked his father, "How were people born?"
+    So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."
+    The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
+    The child ran back to his father and said, "You lied to me!"
+    His father replied, "No, your mom was talking about her side of the family."
+  },
+  %{
+  	Teacher: "Kids,what does the chicken give you?"
+    Student: "Meat!"
+    Teacher: "Very good! Now what does the pig give you?"
+    Student: "Bacon!"
+    Teacher: "Great! And what does the fat cow give you?"
+    Student: "Homework!"
+  },
+  %{
+  	The teacher asked Jimmy, "Why is your cat at school today Jimmy?"
+    Jimmy replied crying, "Because I heard my daddy tell my mommy, 'I am going to eat that pussy once Jimmy leaves for school today!'"
+  }
+].each do |content|
+	Joke.where(content: content).first_or_create!
+end
